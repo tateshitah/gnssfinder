c98ee7f693873d3a6164cc666a33a3871ccf8b3d
unknown <01028@SNO-03.fsad.in-jaxa>
2014-04-06 09:31:49 +0900

    modify some initial error.

--- gnss_webclient/WebContent/js/gnss_GoogleMap_sample01.js
+++ gnss_webclient/WebContent/js/gnss_GoogleMap_sample01.js
@@ -6,72 +6,72 @@
  * http://www.ajaxtower.jp/googlemaps/ Reference from
  * https://developers.google.com/maps/documentation/javascript/events?hl=ja
  * 
+ * 仕様 google mapの任意の点をクリックすると、値が情報ウィンドウに表示される。
  * 
  * 
  */
 
 var map;
 var latlng = "36.068235,140.129172";// 地図の初期値
-var url_DataSource = "prc";
-var url_Date ="2012-08-20";
-var token ="";//トークンを設定
+var url_Date = "2012-08-20";
+var url_Time = "12:00:00";
 
 function initialize() {
 
 	/* Setting for initial map info. */
 	var mapOptions = {
-			zoom: 2,
-			center: new google.maps.LatLng(36.068235,140.129172),
-			mapTypeId: google.maps.MapTypeId.ROADMAP
+		zoom : 2,
+		center : new google.maps.LatLng(36.068235, 140.129172),
+		mapTypeId : google.maps.MapTypeId.ROADMAP
 	};
 
 	/* Generating map */
-	map = new google.maps.Map(document.getElementById('map_canvas'),
-			mapOptions);
+	map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 
-	/* Event when click */  
+	/* Event when click */
 	google.maps.event.addListener(map, 'click', function(event) {
 		latlng = event.latLng;
 		var url_lat = latlng.lat();
 		var url_lng = latlng.lng();
 		var url_Date_temp = $('#datepicker').val();
 		// alert("date_temp "+url_Date_temp);
-		if(url_Date_temp!=""){
+		var url_Time_temp = $('#timepicker').val();
+		// alert("time_temp !? "+url_Time_temp);
+		if (url_Date_temp != "") {
 			url_Date = url_Date_temp;
 		}
-		url_DataSource=$('#sel1').val();
-		var url =  "http://braincopy.org/gnssws/az_and_el?dateTime="+
-		url_Date+"_12:00:00&gnss=J&lat="+url_lat+"&lon="+url_lng+"&format=jsonp";
-		// var url = "http://localhost:8080/api/" + url_DataSource +
-		// "all?token=aaa&date="+ url_Date + "&lat=" + url_lat + "&lon=" +
-		// url_lng + "&format=jsonp";
-		// alert(url);
+		if (url_Time_temp != "") {
+			url_Time = url_Time_temp;
+		}
+		var url = "http://braincopy.org/gnssws/az_and_el?dateTime=" + url_Date
+				+ "_" + url_Time + "&gnss=J&lat=" + url_lat + "&lon=" + url_lng
+				+ "&format=jsonp";
 		load_src(url);
 	});
 
 }
 
-window.callback = function(data){
-	addInfowindow(data.values[0].SatObservation.ObType.ObType1.Elevation,latlng);
-	
+window.callback = function(data) {
+	addInfowindow("el: "+data.values[0].SatObservation.ObType.ObType1.Elevation
+			+"<br />az: "+data.values[0].SatObservation.ObType.ObType1.Azimuth,
+			latlng);
+
 };
 
-function load_src(url){
+function load_src(url) {
 	var script = document.createElement('script');
 	script.src = url;
 	document.body.appendChild(script);
 }
 
-function addInfowindow(text, latLng){
+function addInfowindow(text, latLng) {
 	/* Add information window at click point */
-	var text2 = url_Date+":= " + text;
+	var text2 = url_Date + " " + url_Time + "<BR />" + text;
 	var infowindow = new google.maps.InfoWindow({
-		content: text2,
-		position: latLng
+		content : text2,
+		position : latLng
 	});
 	infowindow.open(map);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-
-
