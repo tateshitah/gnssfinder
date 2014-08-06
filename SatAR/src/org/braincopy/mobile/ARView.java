d7c6ebdb2530f031b967d5604a8b9105b58868cc
Hiroaki Tateshita <tateshita.hiroaki@gmail.com>
2014-08-07 00:30:11 +0900

    update for rolling: still in works. <- add more vertical and horizontal lines.

--- SatAR/src/org/braincopy/mobile/ARView.java
+++ SatAR/src/org/braincopy/mobile/ARView.java
@@ -15,6 +15,7 @@ import android.view.View;
 public class ARView extends View {
 	Paint paint;
 	float lat, lon;
+	int width = 0, height = 0;
 
 	/**
 	 * [degree]
@@ -55,6 +56,8 @@ public class ARView extends View {
 
 	@Override
 	protected void onDraw(Canvas canvas) {
+		width = canvas.getWidth();
+		height = canvas.getHeight();
 
 		canvas.drawText("Direction: " + direction, 50, 50, paint);
 		canvas.drawText("Pitch: " + pitch, 50, 100, paint);
@@ -62,18 +65,17 @@ public class ARView extends View {
 		canvas.drawText("Lat: " + lat, 50, 200, paint);
 		canvas.drawText("Lon: " + lon, 50, 250, paint);
 
-		float height, startX, stopX, startY, stopY;
-
 		// draw horizon
-		height = (float) (canvas.getHeight() * (0.5 + pitch
-				/ (vVeiwAngle * 0.5) * 0.5));
-		startX = 0;
-		stopX = canvas.getWidth();
-		startY = height;
-		stopY = height;
-		canvas.drawLine(startX, startY, stopX, stopY, paint);
+		drawHorizon(canvas, paint);
+		draw30Line(canvas, paint);
+		draw60Line(canvas, paint);
 
-		drawDirection(canvas, paint, height);
+		/*
+		 * height = (float) (canvas.getHeight() * (0.5 + pitch / (vVeiwAngle *
+		 * 0.5) * 0.5)); startX = 0; stopX = canvas.getWidth(); startY = height;
+		 * stopY = height; canvas.drawLine(startX, startY, stopX, stopY, paint);
+		 */
+		drawDirection(canvas, paint);
 		drawSatellites(canvas, paint);
 		drawStatus(canvas, paint);
 		drawTest(canvas, paint, 0, 30);
@@ -85,10 +87,8 @@ public class ARView extends View {
 	}
 
 	private void drawTest(Canvas canvas, Paint paint, float az, float el) {
-		canvas.drawText("(" + az + "," + el + ")",
-				convertAzElX(az, el, canvas.getWidth(), canvas.getHeight()),
-				convertAzElY(az, el, canvas.getWidth(), canvas.getHeight()),
-				paint);
+		canvas.drawText("(" + az + "," + el + ")", convertAzElX(az, el),
+				convertAzElY(az, el), paint);
 	}
 
 	private void drawSatellites(Canvas canvas, Paint paint) {
@@ -131,53 +131,71 @@ public class ARView extends View {
 		}
 	}
 
-	private void drawDirection(Canvas canvas, Paint paint, float height) {
+	private void drawHorizon(Canvas canvas, Paint paint) {
+		float startX, stopX, startY, stopY;
+		for (int i = 0; i < 4; i++) {
+			startX = convertAzElX(i * 90, 0);
+			startY = convertAzElY(i * 90, 0);
+			stopX = convertAzElX((i + 1) * 90, 0);
+			stopY = convertAzElY((i + 1) * 90, 0);
+			canvas.drawLine(startX, startY, stopX, stopY, paint);
+		}
+	}
+
+	private void draw30Line(Canvas canvas, Paint paint) {
+		float startX, stopX, startY, stopY;
+		for (int i = 0; i < 8; i++) {
+			startX = convertAzElX(i * 45, 30);
+			startY = convertAzElY(i * 45, 30);
+			stopX = convertAzElX((i + 1) * 45, 30);
+			stopY = convertAzElY((i + 1) * 45, 30);
+			canvas.drawLine(startX, startY, stopX, stopY, paint);
+		}
+	}
+
+	private void draw60Line(Canvas canvas, Paint paint) {
+		float startX, stopX, startY, stopY;
+		for (int i = 0; i < 8; i++) {
+			startX = convertAzElX(i * 45, 60);
+			startY = convertAzElY(i * 45, 60);
+			stopX = convertAzElX((i + 1) * 45, 60);
+			stopY = convertAzElY((i + 1) * 45, 60);
+			canvas.drawLine(startX, startY, stopX, stopY, paint);
+		}
+	}
+
+	private void drawDirection(Canvas canvas, Paint paint) {
 		float startX, stopX, startY, stopY;
 
 		// draw west
-		startX = (float) (canvas.getWidth() * (0.5 - (direction - 180)
-				/ (hVeiwAngle * 0.5) * 0.5));
-		stopX = (float) (canvas.getWidth() * (0.5 - (direction - 180)
-				/ (hVeiwAngle * 0.5) * 0.5));
-		startY = 100;
-		stopY = height;
+		startX = convertAzElX(180, 0);
+		startY = convertAzElY(180, 0);
+		stopX = convertAzElX(180, 90);
+		stopY = convertAzElY(180, 90);
 		canvas.drawLine(startX, startY, stopX, stopY, paint);
 		canvas.drawText("W", startX, startY, paint);
 
 		// draw south
-		startX = (float) (canvas.getWidth() * (0.5 - (direction - 90)
-				/ (hVeiwAngle * 0.5) * 0.5));
-		stopX = (float) (canvas.getWidth() * (0.5 - (direction - 90)
-				/ (hVeiwAngle * 0.5) * 0.5));
-		startY = 100;
-		stopY = height;
+		startX = convertAzElX(90, 0);
+		startY = convertAzElY(90, 0);
+		stopX = convertAzElX(90, 90);
+		stopY = convertAzElY(90, 90);
 		canvas.drawLine(startX, startY, stopX, stopY, paint);
 		canvas.drawText("S", startX, startY, paint);
 
 		// draw east
-		if (direction > 270) {
-			startX = (float) (canvas.getWidth() * (0.5 - (direction - 360)
-					/ (hVeiwAngle * 0.5) * 0.5));
-			stopX = (float) (canvas.getWidth() * (0.5 - (direction - 360)
-					/ (hVeiwAngle * 0.5) * 0.5));
-		} else if (direction < 90) {
-			startX = (float) (canvas.getWidth() * (0.5 - direction
-					/ (hVeiwAngle * 0.5) * 0.5));
-			stopX = (float) (canvas.getWidth() * (0.5 - direction
-					/ (hVeiwAngle * 0.5) * 0.5));
-		}
-		startY = 100;
-		stopY = height;
+		startX = convertAzElX(0, 0);
+		startY = convertAzElY(0, 0);
+		stopX = convertAzElX(0, 90);
+		stopY = convertAzElY(0, 90);
 		canvas.drawLine(startX, startY, stopX, stopY, paint);
 		canvas.drawText("E", startX, startY, paint);
 
 		// draw north
-		startX = (float) (canvas.getWidth() * (0.5 - (direction - 270)
-				/ (hVeiwAngle * 0.5) * 0.5));
-		stopX = (float) (canvas.getWidth() * (0.5 - (direction - 270)
-				/ (hVeiwAngle * 0.5) * 0.5));
-		startY = 100;
-		stopY = height;
+		startX = convertAzElX(270, 0);
+		startY = convertAzElY(270, 0);
+		stopX = convertAzElX(270, 90);
+		stopY = convertAzElY(270, 90);
 		canvas.drawLine(startX, startY, stopX, stopY, paint);
 		canvas.drawText("N", startX, startY, paint);
 
@@ -201,12 +219,12 @@ public class ARView extends View {
 	 *            [degree]
 	 * @return
 	 */
-	protected float convertAzElX(float azimuth, float elevation, float width,
-			float height) {
+	protected float convertAzElX(float azimuth, float elevation) {
 		float result = (float) (0.5f * width + 0.5f * width
-				* (azimuth - direction) * Math.cos(roll / 180 * Math.PI)
-				/ hVeiwAngle - 0.5f * height * (pitch - elevation)
-				* Math.sin(roll / 180 * Math.PI) / vVeiwAngle);
+				* plusMinus180(azimuth - direction)
+				* Math.cos(roll / 180 * Math.PI) / (0.5f * hVeiwAngle) + 0.5f
+				* height * plusMinus180(pitch - elevation)
+				* Math.sin(roll / 180 * Math.PI) / (0.5f * vVeiwAngle));
 		return result;
 	}
 
@@ -218,13 +236,28 @@ public class ARView extends View {
 	 *            [degree]
 	 * @return
 	 */
-	protected float convertAzElY(float azimuth, float elevation, float width,
-			float height) {
-		float result = (float) (0.5f * height + 0.5f * width
-				* (azimuth - direction) * Math.sin(roll / 180 * Math.PI)
-				/ hVeiwAngle + 0.5f * height * (pitch - elevation)
-				* Math.cos(roll / 180 * Math.PI) / vVeiwAngle);
+	protected float convertAzElY(float azimuth, float elevation) {
+		float result = (float) (0.5f * height - 0.5f * width
+				* plusMinus180(azimuth - direction)
+				* Math.sin(roll / 180 * Math.PI) / (0.5f * hVeiwAngle) + 0.5f
+				* height * plusMinus180(pitch - elevation)
+				* Math.cos(roll / 180 * Math.PI) / (0.5f * vVeiwAngle));
 		return result;
+	}
+
+	/**
+	 * 
+	 * @param in
+	 *            [degree]
+	 * @return
+	 */
+	private float plusMinus180(float in) {
+		if (in < -180) {
+			in += 360;
+		} else if (in > 180) {
+			in -= 360;
+		}
+		return in;
 	}
 
 	public Satellite[] getSatellites() {
