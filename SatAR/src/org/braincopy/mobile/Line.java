dc3f63216a6c68dcdc750571d4e98a10bee45fba
Hiroaki Tateshita <tateshita.hiroaki@gmail.com>
2014-08-11 21:30:12 +0900

    rigorous calculation for azimuth elevation. still working. spatial coordinate system.

---
+++ SatAR/src/org/braincopy/mobile/Line.java
@@ -0,0 +1,11 @@
+package org.braincopy.mobile;
+
+public class Line {
+	// x/a = y/b = z/c
+	float a, b, c;
+
+	public Line() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ SatAR/src/org/braincopy/mobile/Plane.java
@@ -0,0 +1,23 @@
+package org.braincopy.mobile;
+
+public class Plane {
+	// ax + by + cz = d
+	float a, b, c, d;
+
+	public Plane(float a_, float b_, float c_, float d_) {
+		this.a = a_;
+		this.b = b_;
+		this.c = c_;
+		this.d = d_;
+	}
+
+	public Point getIntersection(Line line) {
+		float t = 0;
+
+		t = this.d / (this.a * line.a + this.b * line.b + this.c * line.c);
+		Point result = new Point(line.a * t, line.b * t, line.c * t);
+
+		return result;
+	}
+
+}
