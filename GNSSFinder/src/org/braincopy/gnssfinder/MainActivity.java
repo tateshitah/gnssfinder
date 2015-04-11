d6c57859aba52bba5ce68322b9c3204488a85789
Hiroaki Tateshita <tateshita.hiroaki@gmail.com>
2015-04-11 23:40:10 +0900

    update for release

--- GNSSFinder/src/org/braincopy/gnssfinder/MainActivity.java
+++ GNSSFinder/src/org/braincopy/gnssfinder/MainActivity.java
@@ -1,10 +1,23 @@
 package org.braincopy.gnssfinder;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 //import android.support.v4.app.Fragment;
 
@@ -50,9 +63,59 @@ public class MainActivity extends Activity {
 			this.moveTaskToBack(true);
 			return true;
 		} else if (id == R.id.action_info) {
+			DialogFragment newFragment = new InformationDialogFragment();
+			newFragment.show(getFragmentManager(), "test");
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
 
+	public static class InformationDialogFragment extends DialogFragment {
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Use the Builder class for convenient dialog construction
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+			LayoutInflater inflater = (LayoutInflater) getActivity()
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			View content = inflater.inflate(R.layout.dialog_info, null);
+			InputStream is = null;
+			BufferedReader br = null;
+			String text = "";
+
+			// assetsフォルダ内の sample.txt をオープンする
+			try {
+				is = getActivity().getAssets().open("Info.txt");
+				br = new BufferedReader(new InputStreamReader(is));
+
+				// １行ずつ読み込み、改行を付加する
+				String str;
+				while ((str = br.readLine()) != null) {
+					text += str + "\n";
+				}
+				if (is != null)
+					is.close();
+				if (br != null)
+					br.close();
+			} catch (IOException e) {
+				System.err.println("exception when trying to open Info.txt"
+						+ e.getMessage());
+				e.printStackTrace();
+			}
+			TextView infoTextView = (TextView) content
+					.findViewById(R.id.infoTextView);
+			infoTextView.setText(text);
+
+			builder.setView(content);
+
+			builder.setMessage("About GNSSFinder").setNegativeButton("OK",
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							// User cancelled the dialog
+						}
+					});
+			// Create the AlertDialog object and return it
+			return builder.create();
+		}
+	}
 }
