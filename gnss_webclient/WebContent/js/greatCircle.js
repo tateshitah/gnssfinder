9e8648133f9035e98d125d93ad90313ddf1766fa
Hiroaki Tateshita <tateshita.hiroaki@gmail.com>
2019-01-29 08:22:11 +0900

    clean source code of greatCircle.js

--- gnss_webclient/WebContent/js/greatCircle.js
+++ gnss_webclient/WebContent/js/greatCircle.js
@@ -1,6 +1,7 @@
 
 /**
- * 
+ * use this function when you want to cross date line.
+ * this function can divide linestring on the point on 180 degree.
  */
 let groundTrackFeatures = function (ele) {
     let result = new Array();
@@ -14,13 +15,6 @@ let groundTrackFeatures = function (ele) {
      */
     let direction = -9999;
 
-    /**
-     * last_direction:
-     * 1 -> east to west (ex. -179 -> 179)
-     * -1 -> west to east (ex. 179 -> -179)
-     */
-    let last_direction = -9999;
-
     let cursor = 0;
     ele.forEach(function (v, i, a) {
         if (i > 0) {
@@ -31,12 +25,7 @@ let groundTrackFeatures = function (ele) {
 
             } else if (v[0] - a[i - 1][0] > 180) {
                 direction = 1;
-                //a[i-1][0] less than 0 so ...
-                /*
-                const delta_lon = v[0] - 360 - a[i - 1][0];
-                const delta_lat = v[1] - a[i - 1][1];
-                lat_on_180 = v[1] - (-180 + v[0]) * delta_lat / delta_lon;
-*/
+
                 cursor = devideArray(cursor, direction, result, v, i, a);
             } else {
                 //       alert("hey #8:(" + v[0] + ", " + v[1] + ")");
@@ -45,17 +34,9 @@ let groundTrackFeatures = function (ele) {
         } if (i == ele.length - 1) {
             // alert("hey #9: " + ele.length);
             let tempArray2 = new Array();
-            if (cursor > 0) {
-                let lastArray = result[result.length - 1];
-                let lastCoord = lastArray[lastArray.length - 1];
-                tempArray2.push([
-                    lastCoord[0] * -1,
-                    lastCoord[1]
-                ]);
-            }
-            for (let k = cursor; k < ele.length; k++) {
-                tempArray2.push(ele[k]);
-            }
+
+            pushArray(cursor, result, tempArray2,ele.length,ele);
+
             result.push(tempArray2);
 
         }
@@ -73,18 +54,33 @@ let devideArray = function (cursor, direction, result, v, i, a) {
     const delta_lat = v[1] - a[i - 1][1];
     lat_on_180 = v[1] - (- direction * 180 + v[0]) * delta_lat / delta_lon;
 
+    pushArray(cursor, result, tempArray,i,a);
+    tempArray.push([-1 * 180 * direction, lat_on_180])
+    result.push(tempArray);
+    return i;
+}
+
+/**
+ * 
+ * @param {*} cursor 
+ * @param {*} result 
+ * @param {*} tempArray 
+ * @param {*} i 
+ * @param {*} a 
+ */
+
+let pushArray = function(cursor, result, tempArray, i, a){
+    
     if (cursor > 0) {
         let lastArray = result[result.length - 1];
         let lastCoord = lastArray[lastArray.length - 1];
         tempArray.push([
             lastCoord[0] * -1,
             lastCoord[1]
-        ]);
+        ]);//lastCoord[0] should 180 or -180
     }
     for (let j = cursor; j < i; j++) {
         tempArray.push(a[j]);
     }
-    tempArray.push([-1 * 180 * direction, lat_on_180])
-    result.push(tempArray);
-    return i;
+    
 }
